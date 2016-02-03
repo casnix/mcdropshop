71f568bb5796334416022834e5f54115cfb2c541
Matthew Rienzo <Matthew Rienzo>
2016-02-03 11:33:35 -0600

    Start

---
+++ src/main/java/io/github/casnix/mcdropshop/Main.java
@@ -0,0 +1,21 @@
+/*File Main.java - Part of mcDropShop
+ * Version 0.0.1-SNAPSHOT
+ * Copyright (c) Matt Rienzo, 2016
+ * Licensed under GPLv2.  See license in src/doc/LICENSE or src/doc/LICENSE.txt
+ */
+
+package io.github.casnix.mcdropshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+}
